2c748d4c0d4002bd62a0967a08a9d5468752b489
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-06-30 22:35:01 +0300

    Refactor auth tests

--- src/api/auth/login.test.ts
+++ src/api/auth/login.test.ts
@@ -1,21 +1,20 @@
 import { FastifyInstance } from "fastify";
-import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { build } from "../../app";
 import { getUserIdPlugin } from "../../utils/sessionUtils";
 import { database } from "../../utils/mockDatabase";
 import { hasKey } from "../../types/types";
 
-describe("login", () => {
+describe("login", async () => {
+  const app = await build();
+  await database.signUp("myUsername", "myPassword");
+  await app.register(getUserIdPlugin, { prefix: "/session" });
+
   beforeEach(async () => {
     await database.reset();
   });
 
-  afterEach(async () => {
-    await database.reset();
-  });
-
   test("should return 400 bad request if password is missing", async () => {
-    const app = await build();
     const response = await app.inject({
       method: "POST",
       url: "/auth/login",
@@ -28,7 +27,6 @@ describe("login", () => {
   });
 
   test("should return 400 bad request if username is missing", async () => {
-    const app = await build();
     const response = await app.inject({
       method: "POST",
       url: "/auth/login",
@@ -52,29 +50,21 @@ describe("login", () => {
   };
 
   test("should return 401 if password is incorrect", async () => {
-    await database.signUp("myUsername", "myPassword");
-    const app = await build();
     const response = await login(app, "myUsername", "wrongPassword");
     expect(response.statusCode).toBe(401);
   });
 
   test("should return 401 if user doesn't exist", async () => {
-    const app = await build();
-    const response = await login(app);
+    const response = await login(app, "shrek", "myPassword");
     expect(response.statusCode).toBe(401);
   });
 
   test("should return 200 and user data if credentials are correct", async () => {
-    await database.signUp("myUsername", "myPassword");
-    const app = await build();
     const response = await login(app);
     expect(response.statusCode).toBe(200);
   });
 
   test("when using correct credentials, cookie should be returned", async () => {
-    await database.signUp("myUsername", "myPassword");
-    const app = await build();
-    await app.register(getUserIdPlugin, { prefix: "/session" });
 
     const loginResponse = await login(app);
 
@@ -95,10 +85,6 @@ describe("login", () => {
   });
 
   test("when using wrong credentials, cookie should not be returned", async () => {
-    await database.signUp("myUsername", "myPassword");
-    const app = await build();
-    await app.register(getUserIdPlugin, { prefix: "/session" });
-
     const loginResponse = await login(app, "myUsername", "wrongPassword");
 
     const sessionResponse = await app.inject({
