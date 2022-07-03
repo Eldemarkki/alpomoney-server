c47dfffff30a42b891006244583ca85c4edad8d2
Eldemarkki <38920928+Eldemarkki@users.noreply.github.com>
2022-07-03 16:26:05 +0300

    refactor(database): set dates in database schema to @db.Date

---
+++ prisma/migrations/20220703130619_change_dates_to_date/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "RecurringTransaction" ALTER COLUMN "startDate" SET DATA TYPE DATE;
+
+-- AlterTable
+ALTER TABLE "Transaction" ALTER COLUMN "createdAt" SET DATA TYPE DATE;
